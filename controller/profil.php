2ed60a264bb1789334d22e205b7696b3be12fd00
Luis <tennis.bresil@gmail.com>
2020-03-31 10:59:02 -0400

    ESPACAMENTO

--- controller/profil.php
+++ controller/profil.php
@@ -1,27 +1,27 @@
 <?php
-include '../model/Profil.class.php';
-include '../dao/ProfilDAO.class.php';
-// GET DATA FROM FORM
-extract($_POST);
+	include '../model/Profil.class.php';
+	include '../dao/ProfilDAO.class.php';
+	// GET DATA FROM FORM
+	extract($_POST);
 
-switch ($action) 
-{
-	case 'insert':
-			$profil    = new Profil(null, $ProfilNom);
-			$profilDAO = new ProfilDAO();			
-			$profilDAO->insert($profil);
-			echo"Insertion done";
-		break;
-	case 'update':
-		echo "Données mis à jour ";
-		break;
-	case 'delete':
-		echo "delete ";
-		break;
-	default:
-		echo "none";
-		break;
-}
+	switch ($action) 
+	{
+		case 'insert':
+				$profil    = new Profil(null, $ProfilNom);
+				$profilDAO = new ProfilDAO();			
+				$profilDAO->insert($profil);
+				echo"Insertion done";
+			break;
+		case 'update':
+			echo "Données mis à jour ";
+			break;
+		case 'delete':
+			echo "delete ";
+			break;
+		default:
+			echo "none";
+			break;
+	}
 
 ?>
 
