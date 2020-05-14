7990cb8093028539fa6e5220ece0a883fb6db700
Luis <tennis.bresil@gmail.com>
2020-05-14 09:41:32 -0400

    Update jqury version
    add

--- view/login/index.php
+++ view/login/index.php
@@ -27,7 +27,7 @@
                     </button> 
               </form> 
         </div> 
-        <script src="../../plugins/jquery/jquery-3.4.1.min.js"></script>
+        <script src="../../plugins/jquery/jquery-3.5.1.min.js"></script>
         <script src="../../plugins/popper.min.js"></script>        
         <script src="../../plugins/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script><!-- Bootstrap js -->        
         <script type="text/javascript" src="js/loginScript.js"></script> <!-- JS DU MODULE-->
